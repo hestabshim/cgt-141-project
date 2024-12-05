1c8756d933b1365e215abd1fdf7a16fab47e37ab
Amber <amberzeng@gmail.com>
2024-12-05 07:07:43 -0500

    pls work

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "username: " . $row["username"]. " - password: " . $row["password"]. "<br>";
   }
 } else {
   echo "0 results";
