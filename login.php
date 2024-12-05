133079690d8642ff4cf0ffef11bb640b93a353a5
Amber <amberzeng@gmail.com>
2024-12-05 06:19:50 -0500

    pls work

--- login.php
+++ login.php
@@ -1,18 +1,25 @@
 <?php
 session_start();
+$servername='localhost';
+$name='root';
+$pass='b0518ae66817b1445350179bfb2660a2065daa34703d56c0';
 
-$name="username";
-$pass="password";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $name = htmlspecialchars($_REQUEST['username']);
-    if (($name = empty(1)) && ($pass = empty(1))) {
-        header:('Location: http://165.227.98.100/index.html');
-    }
-    else {
-        header('Location: http://165.227.98.100/classes.html');
-    }
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+  echo "Connected successfully";
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+//     $name = htmlspecialchars($_REQUEST['username']);
+//     if (($name = empty(1)) && ($pass = empty(1))) {
+//         header:('Location: http://165.227.98.100/index.html');
+//     }
+//     else {
+//         header('Location: http://165.227.98.100/classes.html');
+//     }
+// }
 
 
