c0b580980a9575908a37df11cbe88e06fbf75132
Amber <amberzeng@gmail.com>
2024-12-08 15:01:48 -0500

    php test on live server

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+$username= $_POST["username"];
+$password= $_POST["password"];
 $servername='127.0.0.1:3306';
 $name='root';
 $pass='b0518ae66817b1445350179bfb2660a2065daa34703d56c0';
@@ -7,14 +9,16 @@ ini_set('display_errors', 1);
 $conn = new mysqli($servername, $name, $pass, 'logins');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}else{
+$stmt = $conn->prepare("insert into logins (username, password)");
+
 }
 echo "Connected successfully" . "<br>";
-
-// echo $username . $password;
 
 $sql = "SELECT * FROM user_info";
 
 $result = $conn->query($sql);
+
 
 if ($result->num_rows > 0) {
   // output data of each row
@@ -27,15 +31,4 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-
-//     $name = htmlspecialchars($_REQUEST['username']);
-//     if (($name = empty(1)) && ($pass = empty(1))) {
-//         header:('Location: http://165.227.98.100/index.html');
-//     }
-//     else {
-//         header('Location: http://165.227.98.100/classes.html');
-//     }
-// }
 ?>
