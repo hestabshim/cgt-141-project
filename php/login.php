4b7681443569aafa8be80addf8c3384c8028fd11
Amber <amberzeng@gmail.com>
2024-12-09 03:04:14 -0500

    pls work php on god

--- php/login.php
+++ php/login.php
@@ -1,23 +1,37 @@
 <?php
+
 session_start();
-$servername='127.0.0.1:3306';
-$name='root';
-$pass='b0518ae66817b1445350179bfb2660a2065daa34703d56c0';
-$email= $_POST["email"];
-$password= $_POST["password"];
-ini_set('display_errors', 1);
-$conn = new mysqli($servername, $name, $pass, 'logins');
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}else{
-echo "Connection Successful";
+include("connection.php");
+include("functions.php");
+$user_data=check_login($con);
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    //something was posted
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if (!empty($email) && !empty($password)) {
+
+        //read from database
+        $query = "select * from user_info where email = '$email' limit 1";
+        $result = mysqli_query($con, $query);
+
+        if ($result) {
+            if ($result && mysqli_num_rows($result) > 0) {
+
+                $email = mysqli_fetch_assoc($result);
+
+                if ($user_data['password'] === $password) {
+
+                    $_SESSION['user_id'] = $user_data['user_id'];
+                    header("Location: index.php");
+                    die;
+                }
+            }
+        }
+
+        echo "wrong username or password!";
+    } else {
+        echo "wrong username or password!";
+    }
 }
-;
-
-$sql = "SELECT * FROM user_info";
-
-$result = $conn->query($sql);
-
-
-
-?>
--- php/registration.php
+++ php/registration.php
@@ -1,9 +1,11 @@
 <?php
 // define variables
+
 session_start();
-$servername='127.0.0.1:3306';
-$name='root';
-$pass='b0518ae66817b1445350179bfb2660a2065daa34703d56c0';
+include("connection.php");
+include("functions.php");
+
+
 $email= $_REQUEST["email"];
 $password= $_REQUEST["password"];
 // displays errors
@@ -12,7 +14,9 @@ ini_set('display_errors', 1);
 $conn = new mysqli($servername, $name, $pass, 'logins');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+
 }
+
 $sql= "INSERT INTO user_info VALUES ('$email', '$password')";
 if(mysqli_query($conn, $sql)){
     echo "<h3> Registration successful.";
