d890f0379f21b8190f7685354fcb8aacfe115a9b
Amber <amberzeng@gmail.com>
2024-12-08 19:43:39 -0500

    php pls work

--- registration.php
+++ registration.php
@@ -7,12 +7,13 @@ $pass='b0518ae66817b1445350179bfb2660a2065daa34703d56c0';
 $email= $_REQUEST["email"];
 $password= $_REQUEST["password"];
 // displays errors
-ini_set('display_errors', 1);
+ini_set(`display_errors`, 1);
+
 $conn = new mysqli($servername, $name, $pass, 'logins');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql="INSERT INTO 'user_info' VALUES ('$email', '$password')";
+$sql= "INSERT INTO user_info VALUES (`$email`, `$password`)";
 if(mysqli_query($conn, $sql)){
     echo "<h3> Registration successful.";
 }
