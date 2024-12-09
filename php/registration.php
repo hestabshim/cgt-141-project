40ad9ba665ce9b4c97bc5424a8dcda783e0637ec
Amber <amberzeng@gmail.com>
2024-12-09 03:20:09 -0500

    PLEASE PLEASE PLEASE WORK PELASEPLEAE PEL

--- php/registration.php
+++ php/registration.php
@@ -16,9 +16,32 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 
 }
+session_start();
 
-$sql= "INSERT INTO user_info VALUES ('$email', '$password')";
-if(mysqli_query($conn, $sql)){
-    echo "<h3> Registration successful.";
-    echo "<a href='index.html'> Return to login page </a>";
-}
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		if(!empty($email) && !empty($password) && !is_numeric($user_id))
+		{
+
+			//save to database
+			$user_id = random_num(20);
+			$query = "insert into users (user_id,email,password) values ('$user_id','$email','$password')";
+
+			mysqli_query($con, $query);
+
+			header("Location: login.php");
+			die;
+		}else
+		{
+			echo "Please enter some valid information!";
+		}
+	}
+?>
